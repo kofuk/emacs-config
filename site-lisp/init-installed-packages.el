4b321db8fbe90a54f1c9ef5c0f0f7f021ea97d55
Koki Fukuda <ko.fu.dev@gmail.com>
2020-03-16 09:11:46 +0900

    Use web-mode for jsx

--- site-lisp/init-installed-packages.el
+++ site-lisp/init-installed-packages.el
@@ -69,7 +69,7 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-auto-close-style 2)
   (setq web-mode-enable-current-element-highlight t))
