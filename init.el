f9cdcd560ae732fa588d49a2c6a818c4cc4fabd0
Koki Fukuda <ko.fu.dev@gmail.com>
2020-08-11 16:38:48 +0900

    Don't call package-initialize on Emacs 27.1+

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+;; For Emacs 27.1+, we don't have to call package-initialize implicitly,
+;; so call it only if running on older Emacs.
+(if (version< "27.1" emacs-version)
+    (package-initialize))
+
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 (setq init-path (file-name-directory load-file-name))
