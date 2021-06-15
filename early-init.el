ae93edb46174790ec773fa10bf1adc3f185e023a
Koki Fukuda <ko.fu.dev@gmail.com>
2021-06-15 21:02:52 +0900

    Improve

--- early-init.el
+++ early-init.el
@@ -1,3 +1,6 @@
+(if (eq system-type 'windows-nt)
+    (cd "~"))
+
 (defconst my-saved-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 (setq gc-cons-threshold most-positive-fixnum)
