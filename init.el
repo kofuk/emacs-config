35c5b6a28473d613afae7f2132fdf329e5974161
Koki Fukuda <ko.fu.dev@gmail.com>
2021-10-21 14:16:04 +0900

    Improve

--- init.el
+++ init.el
@@ -17,56 +17,112 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; Configure theme first (to avoid flicker while startup)
 (use-package modus-themes
   :ensure t
-  :init
-  (setq
-   modus-themes-italic-constructs t
-   modus-themes-bold-constructs t
-   modus-themes-fringes 'intense
-   modus-themes-paren-match '(bold)
-   modus-themes-region '(bg-only)
-   modus-themes-mode-line '(accented borderless))
-  (modus-themes-load-themes)
+  :custom
+  (modus-themes-italic-constructs t)
+  (modus-themes-bold-constructs t)
+  (modus-themes-fringes 'intense)
+  (modus-themes-paren-match '(bold))
+  (modus-themes-region '(bg-only))
+  (modus-themes-mode-line '(accented borderless))
   :config
+  (modus-themes-load-themes)
   (modus-themes-load-vivendi))
 
-;; C/C++ comment style
-(add-hook 'c-mode-common-hook
-          (lambda () (c-toggle-comment-style 1)))
+(use-package autoinsert
+  :config
+  (auto-insert-mode 1)
+  (add-to-list 'auto-insert-alist
+               '(("CMakeLists\\.txt\\'" . "CMake build configuration")
+                 nil "cmake_minimum_required(VERSION 3.15)\n"
+                 "project(" (read-string "Project: ") & ")\n" | -45
+                 _))
+  :custom
+  (auto-insert-directory (locate-user-emacs-file "inserts")))
+
+(use-package cc-vars
+  :defer t
+  :hook
+  ;; comment style
+  (c-mode-common . (lambda () (c-toggle-comment-style -1))))
 
 (use-package clang-format+
+  :defer t
   :ensure t)
 
 (use-package cmake-mode
+  :defer t
   :ensure t)
 
 (use-package company
   :ensure t
   :config
   (global-company-mode 1)
-  (setq company-transformers '(company-sort-by-backend-importance))
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 2)
-  (setq company-selection-wrap-around t)
-  (setq company-deabbrev-downcase nil)
-  (global-set-key (kbd "C-M-i") 'company-complete)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
   (define-key company-active-map (kbd "C-p") #'company-select-previous)
   (define-key company-active-map (kbd "C-h") nil)
-  (define-key company-active-map (kbd "C-S-h") #'company-show-doc-buffer))
+  (define-key company-active-map (kbd "C-S-h") #'company-show-doc-buffer)
+  :bind
+  ("C-M-i" . 'company-complete)
+  :custom
+  (company-transformers '(company-sort-by-backend-importance))
+  (company-idle-delay 0)
+  (company-minimum-prefix-length 2)
+  (company-selection-wrap-around t)
+  (company-deabbrev-downcase nil))
 
 (use-package company-go
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package csharp-mode
-  :ensure t)
+  :ensure t
+  :defer t)
+
+(use-package cua-base
+  :config
+  ;; Rectangle region
+  ;;  I don't have to enable cua-mode only to ues cus's rectangle region functionality,
+  ;;  but it's required to edit keymaps for rectangle region
+  (cua-mode 1)
+  (define-key cua--rectangle-keymap (kbd "C-x C-x") #'cua-rotate-rectangle)
+  :bind
+  ("C-x SPC" . #'cua-set-rectangle-mark)
+  :custom
+  (cua-enable-cua-keys nil))
+
+(use-package cus-edit
+  :config
+  ;; Custom location for Customize
+  (if (file-exists-p (locate-user-emacs-file "custom.el"))
+      (load-file (locate-user-emacs-file "custom.el")))
+  :custom
+  (custom-file (locate-user-emacs-file "custom.el")))
+
+(use-package delsel
+  :config
+  ;; Overwrite selected area
+  (delete-selection-mode 1))
+
+(use-package dired
+  :defer t
+  :hook
+  (dired-mode . (lambda () (dired-hide-details-mode))))
+
+(use-package display-line-numbers
+  :if (version<= "26.0.50" emacs-version)
+  :config
+  (global-display-line-numbers-mode))
 
 (use-package dockerfile-mode
+  :defer t
   :ensure t)
 
 (use-package editorconfig
   :ensure t
+  :defer t
   :config
   (editorconfig-mode 1))
 
@@ -79,208 +135,218 @@
   (if (executable-find "rust-analyzer")
       (add-to-list 'eglot-server-programs '(rust-mode . ("rust-analyzer")))))
 
+(use-package emacs
+  :custom
+  (indicate-empty-lines t)
+  (create-lockfiles nil)
+  ;; Start up screen
+  (inhibit-startup-screen t)
+  (indent-tabs-mode nil)
+  (history-delete-duplicates t)
+  (initial-scratch-message nil)
+  :config
+  ;; Completion
+  (setq completion-ignore-case t)
+  ;; Use Noto font
+  (set-fontset-font t 'unicode "Noto Sans CJK JP")
+  (set-fontset-font t 'symbol "Noto Color Emoji")
+  ;; Simplify window title
+  (setq frame-title-format '("%b - Emacs"))
+  ;; Keep scratch buffer empty
+  ;; Scroll the buffer
+  :bind
+  ;; Disable annoying key
+  ("\C-v" . nil)
+  ("\M-n" . #'scroll-up-line)
+  ("\M-p" . #'scroll-down-line))
+
+(use-package font-lock
+  :custom
+  (font-lock-support-mode 'jit-lock-mode))
+
+(use-package files
+  :custom
+  ;; Backup file
+  (backup-directory-alist `((".*" . ,(locate-user-emacs-file "backups"))))
+  (auto-save-default nil)
+  ;; Don't warn file size up to 100MB.
+  (large-file-warning-threshold  100000000)
+  ;; Make Emacs to put '\n' at the end of file
+  (require-final-newline t)
+  :mode
+  ("PKGBUILD\\'" . shell-script-mode))
+
+(use-package frame
+  :config
+  (set-cursor-color "white"))
+
+(use-package gdb-mi
+  :defer t
+  :hook
+  (gdb-mode . (lambda () (gud-tooltip-mode t)))
+  :config
+  (setq gdb-many-windows t)
+  (setq gdb-use-separate-io-buffer t)
+  (setq gud-tooltip-echo-area t))
+
 (use-package git-gutter
+  :defer t
   :ensure t)
 
 (use-package go-mode
+  :defer t
   :ensure t)
 
 (use-package highlight-indent-guides
   :ensure t
+  :defer t
+  :hook
+  (prog-mode . (lambda () (highlight-indent-guides-mode t))))
+
+;; Backward compatibility
+(use-package linum
+  :if (version< emacs-version "26.0.50")
   :config
-  (add-hook 'prog-mode-hook #'highlight-indent-guides-mode))
+  (global-linum-mode t))
 
 (use-package lua-mode
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package markdown-mode
   :ensure t
+  :defer t
   :hook
   (markdown-mode . (lambda () (require 'hugo-utils))))
 
 (use-package meson-mode
-  :ensure t)
+  :ensure t
+  :defer t)
+
+(use-package minibuffer
+  :custom
+  (read-file-name-completion-ignore-case t))
 
 (use-package moody
   :ensure t
   :config
- (moody-replace-mode-line-buffer-identification)
+  (moody-replace-mode-line-buffer-identification)
   (moody-replace-vc-mode))
 
+(use-package mozc
+  :if (executable-find "mozc_emacs_helper")
+  :ensure t
+  :defer t
+  :custom
+  (default-input-method "japanese-mozc")
+  (mozc-candidate-style 'echo-area))
+
+(use-package newst-reader
+  :defer t
+  :config
+  (load "feeds")
+  (defun news ()
+    (interactive)
+    (newsticker-show-news)))
+
+(use-package paren
+  :config
+  (show-paren-mode t)
+  :custom
+  (show-paren-style 'mixed))
+
 (use-package sass-mode
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package satysfi
   :config
-  (add-to-list 'auto-mode-alist '("\\.saty\\'" . satysfi-mode))
-  (add-to-list 'auto-mode-alist '("\\.satyh\\'" . satysfi-mode))
   (if (equal system-type 'gnu/linux)
-      (setq satysfi-pdf-viewer-command "evince")))
+      (setq satysfi-pdf-viewer-command "evince"))
+  :mode
+  ("\\.saty\\'" . satysfi-mode)
+  ("\\.satyh\\'" . satysfi-mode))
+
+(use-package saveplace
+  :config
+  ;; Save cursor position
+  (if (fboundp 'save-place-mode)
+      (save-place-mode 1)
+    (setq-default save-place t)))
+
+(use-package simple
+  :config
+  (line-number-mode 1)
+  (column-number-mode 1)
+  (size-indication-mode 1))
+
+(use-package so-long
+  :config
+  (global-so-long-mode t))
+
+(use-package subword
+  :config
+  ;; Recognize words in camelCaseString.
+  (global-subword-mode t))
 
 (use-package rust-mode
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package undo-tree
   :ensure t
   :config
-  (setq undo-tree-mode-lighter "")
-  (global-undo-tree-mode t))
+  (global-undo-tree-mode t)
+  :custom
+  (undo-tree-mode-lighter ""))
 
-(if (equal system-type 'gnu/linux)
-    (use-package vterm
-      :ensure t))
+(use-package vc-hooks
+  :custom
+  ;; Follow symbolic links to versioned files
+  (vc-follow-symlinks t))
+
+(use-package verilog-mode
+  :defer t
+  :bind-keymap
+  ;; Disable auto insertion of new line after `;'.
+  (";" . nil))
+
+(use-package vterm
+  :if (equal system-type 'gnu/linux)
+  :ensure t
+  :config
+  (define-key vterm-mode-map (kbd "C-v") nil)
+  (define-key vterm-mode-map (kbd "C-v C-v") #'vterm-send-C-v)
+  (define-key vterm-mode-map (kbd "C-v ESC") #'vterm-copy-mode)
+  (define-key vterm-mode-map (kbd "C-y") #'vterm-yank)
+  (define-key vterm-copy-mode-map (kbd "ESC") #'vterm-copy-mode))
 
 (use-package web-mode
   :ensure t
+  :defer t
+  :mode
+  (("\\.php\\'" . web-mode)
+   ("\\.html?\\'" . web-mode)
+   ("\\.jsx\\'" . web-mode))
+  :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-auto-close-style 2)
+  (web-mode-enable-current-element-highlight t))
+
+(use-package which-func
   :config
-  (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-auto-close-style 2)
-  (setq web-mode-enable-current-element-highlight t))
+  (which-function-mode t))
 
-(if (executable-find "mozc_emacs_helper")
-    (use-package mozc
-      :ensure t
-      :config
-      (setq default-input-method "japanese-mozc")
-      (setq mozc-candidate-style 'echo-area)))
+(use-package whitespace
+  :config
+  (global-whitespace-mode 1)
+  :custom
+  ;; Show whitespaces
+  (whitespace-style '(face trailing tabs tab-mark)))
 
-;;; Other configurations
-
-(setq font-lock-support-mode 'jit-lock-mode)
-(setq font-lock-maximum-decornation t)
-(show-paren-mode t)
-(setq show-paren-style 'mixed)
-
-;; Mode line
-(line-number-mode 1)
-(column-number-mode 1)
-(size-indication-mode 1)
-;; Display battery status only on laptop.
-(if (file-exists-p "/sys/class/dmi/id/chassis_type")
-    (with-temp-buffer
-      (insert-file-contents "/sys/class/dmi/id/chassis_type")
-      (if (not (string= (buffer-string) "3\n"))
-          (display-battery-mode 1))))
-
-(set-cursor-color "white")
-
-(setq-default indicate-empty-lines t)
-
-;; GDB
-(setq gdb-many-windows t)
-(setq gdb-use-separate-io-buffer t)
-(add-hook 'gdb-mode-hook '(lambda () (gud-tooltip-mode t)))
-(setq gud-tooltip-echo-area t)
-
-;; Rectangle region
-;;  I don't have to enable cua-mode only to ues cus's rectangle region functionality,
-;;  but it's required to edit keymaps for rectangle region
-(cua-mode 1)
-(setq cua-enable-cua-keys nil)
-(global-set-key (kbd "C-x SPC") #'cua-set-rectangle-mark)
-(define-key cua--rectangle-keymap (kbd "C-x C-x") #'cua-rotate-rectangle)
-
-;; Show line number in the left
-(if (version<= "26.0.50" emacs-version)
-    (global-display-line-numbers-mode)
-  (global-linum-mode t))
-
-;; Recognize words in camelCaseString.
-(global-subword-mode t)
-
-(global-so-long-mode t)
-
-(which-function-mode t)
-
-;; Start up screen
-(setq inhibit-startup-screen t)
-
-;; Save cursor position
-(if (fboundp 'save-place-mode) (save-place-mode 1) (setq-default save-place t))
-
-;; Follow symbolic links to versioned files
-(setq vc-follow-symlinks t)
-
-;; Backup file
-(setq backup-directory-alist `((".*" . ,(locate-user-emacs-file "backups"))))
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
-
-;; Show whitespaces
-(setq whitespace-style '(face trailing tabs tab-mark))
-(setq-default indent-tabs-mode nil)
-(global-whitespace-mode 1)
-
-(setq read-file-name-completion-ignore-case t)
-(setq completion-ignore-case t)
-(setq history-delete-duplicates t)
-
-(setq initial-scratch-message nil)
-
-;; Don't warn file size up to 100MB.
-(setq large-file-warning-threshold  100000000)
-
-;; Disable annoying key
-(global-set-key "\C-v" nil)
-
-;; Scroll the buffer line-by-line
-(global-set-key "\M-n" #'scroll-up-line)
-(global-set-key "\M-p" #'scroll-down-line)
-
-(defun revert-buffer-noconfirm ()
-  "Reverts buffer data from assciated file, without any prompt"
-  (interactive)
-  (revert-buffer 1 1 1))
-
-(global-set-key (kbd "<f5>") #'revert-buffer-noconfirm)
-
-;; Make Emacs to put '\n' at the end of file
-(setq require-final-newline t)
-
-;; Auto insert
-(auto-insert-mode 1)
-(setq auto-insert-directory (locate-user-emacs-file "inserts"))
-(add-to-list 'auto-insert-alist
-             '(("CMakeLists\\.txt\\'" . "CMake build configuration")
-               nil "cmake_minimum_required(VERSION 3.15)\n"
-               "project(" (read-string "Project: ") & ")\n" | -45
-               _))
-
-;; Major mode
-(add-to-list 'auto-mode-alist '("PKGBUILD\\'" . shell-script-mode))
-
-;; Custom location for Customize
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(if (file-exists-p (locate-user-emacs-file "custom.el"))
-    (load-file (locate-user-emacs-file "custom.el")))
-
-(add-hook 'dired-mode-hook
-          (lambda ()
-            (dired-hide-details-mode)))
-
-;; Disable auto insertion of new line after `;'.
-(add-hook 'verilog-mode-hook
-          (lambda ()
-            (define-key verilog-mode-map ";" nil)))
-
-;; Use Noto font
-(set-fontset-font t 'unicode "Noto Sans CJK JP")
-(set-fontset-font t 'symbol "Noto Color Emoji")
-
-(setq frame-title-format "%b - Emacs")
-
-;; Overwrite selected area
-(delete-selection-mode 1)
-
-(windmove-default-keybindings 'meta)
-
-(load "feeds")
-(defun news ()
-  (interactive)
-  (newsticker-show-news))
+(use-package windmove
+  :config
+  (windmove-default-keybindings 'meta))
 
 (defmacro :? (obj &rest body)
   "Evaluate BODY if OBJ is nil."
@@ -302,6 +368,20 @@ filename#L1-L2 form."
        (format "%s#L%d" file (line-number-at-pos (point))))))
   (deactivate-mark))
 
+(defun revert-buffer-noconfirm ()
+  "Reverts buffer data from assciated file, without any prompt"
+  (interactive)
+  (revert-buffer 1 1 1))
+
+(global-set-key (kbd "<f5>") #'revert-buffer-noconfirm)
+
+;; Display battery status only on laptop.
+(if (file-exists-p "/sys/class/dmi/id/chassis_type")
+    (with-temp-buffer
+      (insert-file-contents "/sys/class/dmi/id/chassis_type")
+      (if (not (string= (buffer-string) "3\n"))
+          (display-battery-mode 1))))
+
 ;; Execute local lisp initialization.
 ;; Execute in the last step of init.el so that it doesn't disturb
 ;; other package's initialization.
@@ -309,7 +389,9 @@ filename#L1-L2 form."
     (load-file (locate-user-emacs-file "init-local.el")))
 
 ;; "Clear the clutter."
-(setq minor-mode-alist nil)
+(use-package emacs
+  :config
+  (setq minor-mode-alist nil))
 
 (setq file-name-handler-alist my-saved-file-name-handler-alist)
 (setq gc-cons-threshold 16777216)
